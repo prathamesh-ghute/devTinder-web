962a8b1cf8bd9292453954da0f10738c5003111f
Prathamesh Ghute <prathameshghute6@gmail.com>
2026-02-03 23:53:40 +0530

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NavBar from "./NavBar"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 
 function App() {
-  
+
 
   return (
     <>
-      <h1>Hello World</h1>
+    <BrowserRouter basename="/">
+      <Routes>
+          <Route path="/" element={<Body />} >
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+          </Route>          
+
+      </Routes>
+    </BrowserRouter>
+     
     </>
   )
 }
